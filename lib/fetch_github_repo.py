88e04c2ce88d702efd960fb2f0b60c2f0dcbdf34
Bryon Smith <bryon@smithauto.us>
2022-06-12 22:39:55 -0500

    added new functions to get data

--- lib/fetch_github_repo.py
+++ lib/fetch_github_repo.py
@@ -1,35 +1,100 @@
-from urllib.request import Request, urlopen
-import base64, json, re
-import pathlib
+import requests, json, pathlib 
+from datetime import datetime
+
+SNAPSHOT_DIR = pathlib.Path.cwd().joinpath('snapshot')
+
+def filterData(data):
+    db = []
+    for d in data:
+        obj = {
+            "name": d['name'], # name of repo
+            "default_branch": d['default_branch'], # master, main ..
+            "updated_at": datetime.strptime(d['updated_at'], "%Y-%m-%dT%H:%M:%SZ").timestamp(),
+            "pushed_at": datetime.strptime(d['pushed_at'], "%Y-%m-%dT%H:%M:%SZ").timestamp()
+        }
+        db.append(obj)
+    return db
 
 
-def get(user, token):
-    URL_USER = 'https://api.github.com/user'
-    URL_REPO = f'https://api.github.com/users/{user}/repos?per_page=100'
+# both args are list of dicts. Keys: name, default_branch, updated_at, pushed_at
+def processLinks(db, data):
+    new_database = []
+    download = []
+    remove_list = []
+    for new_data in data: #loop downloaded data
+        # no matter what add repo to new_database
+        new_database.append(new_data)
+        is_new_repo = True
+        for old_data in db:
+            is_new_repo = True
+            if new_data['name'] == old_data['name']:
+                is_new_repo = False
+                break
 
-    base64string = base64.b64encode(bytes('%s:%s' % (user, token),'ascii'))
+        for old_data in db: # loop database data
+            # new repo name in database?
+            if new_data['name'] == old_data['name']:
+                # exact match?
+                if new_data == old_data:
+                    # do nothing, repo downloaded and new_data already in new_database
+                    continue
+                # was not an exact match
+                else:
+                    remove_list.append(old_data)
+                    download.append(new_data)
+            
+            if is_new_repo:
+                download.append(new_data)
 
-    req =Request(URL_REPO)
-    req.add_header("Authorization", "Basic %s" % base64string.decode('utf-8'))
-    req.add_header("Accept", 'application/vnd.github.v3+json')
-    res = urlopen(req)
-
-    status = res.status
-    print(status)
-
-    # pull all the pages
-    headers = res.getheaders()
-    pathlib.Path.cwd().joinpath('avoid', 'headers.json').write_text(json.dumps(headers, indent=4))
-
-    return json.loads(res.read())
+    print(f'You have {len(new_database)} repositories.')
+    print(len(download))
+    SNAPSHOT_DIR.joinpath('github_data.json').write_text(json.dumps(new_database))
+    return (download, remove_list)
 
 
-def processLinks(linkList):
-    pass
-    #[
-    #  "Link", 
-    # "<https://api.github.com/user/17347091/repos?page=2>; rel=\"next\", 
-    # <https://api.github.com/user/17347091/repos?page=2>; rel=\"last\""
-    #],
 
-    # re
+def downloadRepo(user, name, token, default_branch, write_path):
+    URL = f'https://github.com/{user}/{name}/archive/refs/heads/{default_branch}.zip'
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    res = requests.get(URL, auth=(user, token), headers = headers)
+    # print(res.status_code)
+    # h = res.headers
+    # print(h)
+    with open(f'{write_path}/{name}.zip', 'wb') as b:
+        b.write(res.content)
+
+
+
+def get(user, token, db):
+    #Get all repos, including private and collaborator in another organization.
+    URL_ALL_REPOS = 'https://api.github.com/user/repos?per_page=100&affiliation=owner,collaborator' 
+
+    URL = URL_ALL_REPOS
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    res = requests.get(URL, auth=(user, token), headers = headers)
+    code = res.status_code
+   
+    if code > 304:
+        print(f'Request failed with code: {code}')
+    
+    data = res.json()
+
+    # extract needed data from repo data
+    db_data = filterData(data)
+    download, remove_list = processLinks(db, db_data)
+
+    # ask to remove old repos?
+    if remove_list:
+        remove_files = input('Would you like to remove old repo files? (y/n)')
+        if remove_files:
+            for file in remove_list:
+                SNAPSHOT_DIR.joinpath(file).unlink()
+    
+    if download:
+        print('Starting Download')
+        for d in download:
+            f_path = SNAPSHOT_DIR.joinpath(d['name']+ str(d['updated_at']+'.zip'))
+            print(f_path)
+            # downloadRepo(user, d['name'], token, d['default_branch'], f_path)
+
+    return {} 
